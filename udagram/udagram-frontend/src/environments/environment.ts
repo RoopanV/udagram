413ad76c22699cc49e3e3deae90bdb7e7935c8dc
Roopan <roopan.narayanan@infosys.com>
2022-06-16 23:21:28 +0530

    added front end test cases

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:4000/api/v0'
+  apiHost: 'http://udacitybackendprod-env.eba-zymaw6rt.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
